ec5a1df9126ee57111c6dd445e53830af9006287
russelldub <russelldub@gmail.com>
2014-06-04 22:48:13 -0700

    Redirect "/" to "vmgame"

--- vmgame_website/urls.py
+++ vmgame_website/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
+
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,4 +12,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^vmgame/', include('vmgame.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    #Re-direct from root to vmgame
+    url(r'^$', RedirectView.as_view(url='vmgame/', permanent=False), name='index'),
 )
