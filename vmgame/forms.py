561e94668c0f0286c0d590cc57cf44796490afb2
russelldub <russelldub@gmail.com>
2014-06-05 01:32:35 -0700

    Works for picking the group winners and champion

--- vmgame/forms.py
+++ vmgame/forms.py
@@ -6,19 +6,25 @@ from vmgame.models import Team,Group,Player,Pick
 class PickForm(forms.ModelForm):    
     TEAM_NAME_CHOICES = [(t.__unicode__(),t.__unicode__()) for t in Team.objects.all()]
 
-    champion = forms.CharField(max_length=3, 
+    champion = forms.CharField(max_length=80, 
                         widget=forms.Select(choices=TEAM_NAME_CHOICES), 
                         help_text="Pick the winner of the 2014 world cup")
 #    GROUP_A_CHOICES = teams_in_group(teams, "A")
-    group_winner={}
-    for group_name in ["A", "B", "C", "D", "E", "F", "G", "H"]:
-       gr = Group.objects.filter(name=group_name)
-       group_teams = Team.objects.filter(group=gr)
+    def make_group_select(group_letter):
+       group_teams = Team.objects.filter(group__name="Group {0}".format(group_letter))
        GROUP_TEAM_CHOICES = [(t.__unicode__(),t.__unicode__()) for t in group_teams]
-       #group_winner[group_name] = forms.CharField(max_length=3, 
-       group_winner_test = forms.CharField(max_length=3, 
-                        widget=forms.Select(choices=GROUP_TEAM_CHOICES), 
-                        help_text="Pick the winner of group {0}".format(group_name))
+       return forms.CharField(max_length=80, 
+                              widget=forms.Select(choices=GROUP_TEAM_CHOICES), 
+                              help_text="Pick the winner of Group {0}".format(group_letter))
+
+    groupA = make_group_select("A");
+    groupB = make_group_select("B");
+    groupC = make_group_select("C");
+    groupD = make_group_select("D");
+    groupE = make_group_select("E");
+    groupF = make_group_select("F");
+    groupG = make_group_select("G");
+    groupH = make_group_select("H");
 
     points = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     completed = forms.BooleanField(widget=forms.HiddenInput(), initial=False)
@@ -28,7 +34,8 @@ class PickForm(forms.ModelForm):
             model = Pick
 
             #fields = ('group_winners', "champion")
-            fields = ("champion","group_winner_test")
+            fields = ("champion","groupA","groupB","groupC",
+                      "groupD","groupE","groupF","groupG","groupH")
 
     def clean(self):
         cleaned_data = self.cleaned_data
