43e5219566b15ddeac350edbf467b788fc6371b4
Nick <fossaone@gmail.com>
2014-06-14 18:02:06 +0000

    removed link to enterpicks

--- vmgame/urls.py
+++ vmgame/urls.py
@@ -5,12 +5,12 @@ from vmgame import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name = 'index'),
     #url(r'^(?P<vmgame_pick_id>\d+)/$', views.enterpicks, name='enterpicks'),
-    url(r'^enterpicks/', views.enterpicks, name='enterpicks'),
+    #url(r'^enterpicks/', views.enterpicks, name='enterpicks'),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
     #url(r'^displaypicks/(?P<user_picks_url>\w+)/$', views.displaypicks, name='displaypicks'),
     url(r'^displaypicks/$', views.displaypicks, name='displaypicks'),
     url(r'^displaypicks/(?P<user_pick_id>\w+)$', views.displaypicks, name='display_user_pick'),
     url(r'^logout/$', views.user_logout, name='logout'),
-    
+
 )
