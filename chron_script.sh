1a64507ba913dc6ec21bbff373c5fa966e7333e5
Nick <fossaone@gmail.com>
2014-06-24 17:35:54 +0000

    Fix chron_script.sh to run in correct directory

--- chron_script.sh
+++ chron_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#For some reason pythonanywhere runs this from the home directory
+cd /home/vmgame2014/vmgame
+
 #Don't update database if update_results.sh fails
 set -e
 date >> update_vmgame_results.log
@@ -11,5 +14,4 @@ cd ../
 
 #Go from text files to database
 ./update_vmgame_results.py >> update_vmgame_results.log
-
 
