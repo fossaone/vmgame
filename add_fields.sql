c4e03c85d9b03675725c87dfc4b6ebbba9219c6f
Nick <fossaone@gmail.com>
2014-06-24 17:35:26 +0000

    Comment out add field commands but retain file for future reference.

--- add_fields.sql
+++ add_fields.sql
@@ -1,6 +1,5 @@
 
-# SQLite commands to add a column
-
-ALTER TABLE "vmgame_team" ADD COLUMN "country_regularized" varchar(80);
-ALTER TABLE "vmgame_player" ADD COLUMN "name_regularized" varchar(80);
+--  Code retained as an example of how to add a new field to a model manually
+--ALTER TABLE "vmgame_team" ADD COLUMN "country_regularized" varchar(80);
+--ALTER TABLE "vmgame_player" ADD COLUMN "name_regularized" varchar(80);
 
