0a54e76925e9ea55539ca64e02d30207ed590164
Nick <fossaone@gmail.com>
2014-06-29 23:12:10 +0000

    Direct error output to log for chron_script.sh

--- chron_script.sh
+++ chron_script.sh
@@ -14,9 +14,9 @@ date >> update_vmgame_results.log
 
 #Grab latest wikipedia and process
 cd ext
-./update_results.sh >> ../update_vmgame_results.log
+./update_results.sh 2>&1 >> ../update_vmgame_results.log
 cd ../
 
 #Go from text files to database
-./update_vmgame_results.py >> update_vmgame_results.log
+./update_vmgame_results.py 2>&1 >> update_vmgame_results.log
 
