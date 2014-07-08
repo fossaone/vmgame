2d96ba735355d6137bf4d36aacf09cf1a194ec88
Nick <fossaone@gmail.com>
2014-07-08 23:06:09 +0000

    Fix (maybe) err output for updater script

--- chron_script.sh
+++ chron_script.sh
@@ -14,11 +14,11 @@ date >> update_vmgame_results.log
 
 #Grab latest wikipedia and process
 cd ext
-./update_results.sh 2>&1 >> ../update_vmgame_results.log
+./update_results.sh &>> ../update_vmgame_results.log
 cd ../
 
 echo "Starting update_vmgame_results.py" >> update_vmgame_results.log
 #Go from text files to database
-./update_vmgame_results.py 2>&1 >> update_vmgame_results.log
+./update_vmgame_results.py &>> update_vmgame_results.log
 
 echo "Done updating results" >> update_vmgame_results.log
