666ffc27d3155103ef73bfe45ca265e67b4a37e7
Russell Whitesides <russelldub@gmail.com>
2016-06-03 22:44:27 -0700

    Fix group letters and tournament start

--- vmgame/config.py
+++ vmgame/config.py
@@ -3,8 +3,8 @@ import datetime
 
 package_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
-GROUP_LETTERS = ["A","B","C","D","E","F","G","H"]
+GROUP_LETTERS = ["A","B","C","D","E","F"]
 GROUP_RANKS = ["1st","2nd","3rd","4th"]
 
 #UTC
-TOURNAMENT_START = datetime.datetime(2014,6,12,20,0)
+TOURNAMENT_START = datetime.datetime(2016,6,10,19,0)
